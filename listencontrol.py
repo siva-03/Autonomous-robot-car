abc6b54c0acf9c6f84e77067491440793bb89e5e
kinderst <kinders@uw.edu>
2023-05-06 21:08:31 -0600

    lol

--- listencontrol.py
+++ listencontrol.py
@@ -13,7 +13,7 @@ import os
 
 class StopStartData:
     def __init__(self):
-        self.is_started = False
+        self.speed = 1500
 
 
 class SensorData:
@@ -33,7 +33,10 @@ def imu_callback(data, sensor_data):
 
 
 def stop_start_callback(data, stop_start_data):
-    stop_start_data.is_started = not stop_start_data.is_started
+    if data.isdigit():
+        stop_start_data.speed = int(data)
+    else:
+        stop_start_data.speed = 1500
 
 
 def camera_callback(data, param_data):
@@ -156,59 +159,58 @@ def control_loop():
     rospy.sleep(1)
 
     while not rospy.is_shutdown():
-        if start_stop_data.is_started:
-
-            if depth_data.image_data is not None:
-                position = get_difference_with_threshold(depth_data.image_data, threshold)
-                print("im currently at camera diff position: ", position)
-                # Get current time
-                current_time = time.time()
-
-                # Calculate time elapsed since last iteration
-                delta_time = current_time - last_time
-
-                # Calculate error
-                error = set_point - position
-
-                # Calculate derivative of error
-                if delta_time > 0:
-                    error_derivative = (error - last_error) / delta_time
-                else:
-                    error_derivative = 0
-
-                # Calculate integral term
-                integral += error * delta_time
-
-                # Calculate output value
-                output = kp * error + ki * integral + kd * error_derivative
-                print("output before min/max: ", output)
-
-                # Limit output to maximum value
-                output = min(max_output, max(-max_output, output))
-                # get_desired_angle(output) # if output = -0.1, returns -10degree, if output = 0.05, return 6degree
-                # maybe wrong? maybe right? maybe need to keep track of maestro current position
-                # and use the range to be -50 to 50, if x = 20 above, and add or subtract output
-                # from current steering pos <- looks good!
-                # output_to_pololu_value = min_max_scale(output)
-                # convert_output_to_maestro_int # take output, get 1000-2000 value for our steering angle
-                # call_servo_with_int(output) # get byte seq, and write to file
-                print("output after min/max: ", output)
-                maestro_output = min_max_scale(output,
-                                               -max_output,
-                                               max_output,
-                                               (-1000/discretization_amount),
-                                               (1000/discretization_amount))
-                print("maestro output: ", maestro_output)
-                car_current_wheel += maestro_output
-                print("car current wheel after output: ", car_current_wheel)
-                car_current_wheel = min(2000, max(car_current_wheel, 1000))
-                print("car current wheel after CHOPPING: ", car_current_wheel)
-                write_serial_byte_string(channel=2, target=car_current_wheel)
-
-            rospy.sleep(0.05)
-
-        else:
+        write_serial_byte_string(channel=1, target=start_stop_data.speed)
+        if start_stop_data.speed == 1500:
             write_serial_byte_string(channel=2, target=1500)
+
+        if depth_data.image_data is not None:
+            position = get_difference_with_threshold(depth_data.image_data, threshold)
+            print("im currently at camera diff position: ", position)
+            # Get current time
+            current_time = time.time()
+
+            # Calculate time elapsed since last iteration
+            delta_time = current_time - last_time
+
+            # Calculate error
+            error = set_point - position
+
+            # Calculate derivative of error
+            if delta_time > 0:
+                error_derivative = (error - last_error) / delta_time
+            else:
+                error_derivative = 0
+
+            # Calculate integral term
+            integral += error * delta_time
+
+            # Calculate output value
+            output = kp * error + ki * integral + kd * error_derivative
+            print("output before min/max: ", output)
+
+            # Limit output to maximum value
+            output = min(max_output, max(-max_output, output))
+            # get_desired_angle(output) # if output = -0.1, returns -10degree, if output = 0.05, return 6degree
+            # maybe wrong? maybe right? maybe need to keep track of maestro current position
+            # and use the range to be -50 to 50, if x = 20 above, and add or subtract output
+            # from current steering pos <- looks good!
+            # output_to_pololu_value = min_max_scale(output)
+            # convert_output_to_maestro_int # take output, get 1000-2000 value for our steering angle
+            # call_servo_with_int(output) # get byte seq, and write to file
+            print("output after min/max: ", output)
+            maestro_output = min_max_scale(output,
+                                           -max_output,
+                                           max_output,
+                                           (-1000/discretization_amount),
+                                           (1000/discretization_amount))
+            print("maestro output: ", maestro_output)
+            car_current_wheel += maestro_output
+            print("car current wheel after output: ", car_current_wheel)
+            car_current_wheel = min(2000, max(car_current_wheel, 1000))
+            print("car current wheel after CHOPPING: ", car_current_wheel)
+            write_serial_byte_string(channel=2, target=car_current_wheel)
+
+        rospy.sleep(0.05)
 
 
 if __name__ == '__main__':
