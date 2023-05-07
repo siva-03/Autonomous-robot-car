f1e996071446244caf188ada3b1e86e856d91689
kinderst <kinders@uw.edu>
2023-05-07 16:20:31 -0600

    ok

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python
+
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    scripts=['src/car_control/control_callbacks.py',
+             'src/car_control/control_classes.py',
+             'src/car_control/utils.py'],
     packages=['car_control'],
     package_dir={'': 'src'}
 )
 
-setup(**d)
+setup(**setup_args)
