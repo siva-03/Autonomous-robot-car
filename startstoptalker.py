e90f9fc614459571fdfcf244071677586ab7749d
kinderst <kinders@uw.edu>
2023-05-06 20:51:26 -0600

    ok

--- startstoptalker.py
+++ startstoptalker.py
@@ -12,6 +12,7 @@ class StopStartData:
 def talker():
     pub = rospy.Publisher('startstop', String, queue_size=10)
     rospy.init_node('mystartstoptalker', anonymous=True)
+    rate = rospy.Rate(10)  # 10hz
 
     while not rospy.is_shutdown():
         user_input = input()
@@ -19,7 +20,7 @@ def talker():
         print('user input coming in: ', user_input)
         rospy.loginfo(user_input)
         pub.publish(user_input)
-        rospy.spin()
+        rate.sleep()
 
 
 if __name__ == '__main__':
