1eb9223c017227a8a2f56cb1ee29655777a42252
kinderst <kinders@uw.edu>
2023-05-09 14:34:58 -0600

    lol

--- listen_control.py
+++ listen_control.py
@@ -104,7 +104,7 @@ def control_loop():
             middle_third_mean = np.mean(depth_data.image_data[120:420, 213:427])
             print("middle third mean", middle_third_mean)
             continue_script = True
-            if middle_third_mean < 3200:
+            if middle_third_mean < 2000:
                 continue_script = False
 
                 # turn
