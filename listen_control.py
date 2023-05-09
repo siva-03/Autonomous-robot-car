2f7abf7734effc11c00448d3339535b804885cfb
kinderst <kinders@uw.edu>
2023-05-09 01:19:50 -0600

    lol

--- listen_control.py
+++ listen_control.py
@@ -105,7 +105,7 @@ def control_loop():
 
                         position = get_difference_with_threshold(depth_data.image_data[360:, :, :], threshold)
                         print("pos: ", position)
-                        maestro_output = min_max_scale(position, -threshold/3, threshold/3, 1000, 2000)
+                        maestro_output = min_max_scale(position, -threshold/2, threshold/2, 1000, 2000)
                         print('maestro: ', maestro_output)
                         diff = (1500 - maestro_output)
                         final_out = 1500 + diff
