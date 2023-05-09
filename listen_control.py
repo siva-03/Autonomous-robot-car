0e9c9df11864ca806931c26348f1079a1f67ef27
kinderst <kinders@uw.edu>
2023-05-09 01:19:10 -0600

    lol

--- listen_control.py
+++ listen_control.py
@@ -103,7 +103,7 @@ def control_loop():
                             is_stop_sign = stop_sign_detector(camera_data.original_img_cv)
                             print("is stop sign? ", is_stop_sign)
 
-                        position = get_difference_with_threshold(depth_data.image_data, threshold)
+                        position = get_difference_with_threshold(depth_data.image_data[360:, :, :], threshold)
                         print("pos: ", position)
                         maestro_output = min_max_scale(position, -threshold/3, threshold/3, 1000, 2000)
                         print('maestro: ', maestro_output)
