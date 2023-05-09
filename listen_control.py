a28c463d5368912f9a94439d54d4979fda4cc01c
kinderst <kinders@uw.edu>
2023-05-09 14:30:37 -0600

    lol

--- listen_control.py
+++ listen_control.py
@@ -189,7 +189,7 @@ def control_loop():
             print("no depth data, centering car")
             car.center()
 
-        rospy.sleep(0.5)
+        rospy.sleep(0.05)
 
 
 def shutdown_callback():
